613fa9278642f8a8400e4c8af81ba767ff1c5dc7
Duong Mowou <mowou@Duongs-MacBook-Air.local>
2024-01-04 15:58:32 +0700

    Update App.tsx for better view

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Header } from "./components/Header"
-import { ViewThread } from "./components/Posts"
 import { CardPost } from "./components/Home"
-import { Article } from "./model";
 
 const App: React.FC = () => {
-
-  const [article, setArticle] = useState<string>("");
-  const [articles, setArticles] = useState<Article[]>([]);
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault(); 
-    if (article){
-      setArticles([...articles,{ 
-        id:Date.now(), 
-        article,
-        isPublic:false }])
-        setArticle("");
-    }
-  }
-
   return (
     <div className="App">
       <Header></Header>
-      <ViewThread></ViewThread>
+      <CardPost></CardPost>
     </div>
   );
 };
