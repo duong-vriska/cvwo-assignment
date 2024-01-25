1001c764bd4561f4b8959c746db16aadb0bc980b
Duong Ngoc Mai <lazycats121@gmail.com>
2024-01-25 23:49:07 +0800

    Refactor category routes in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Header } from "./components/Header"
 import { PostCreator } from "./components/Creator/index"
 import { ViewThread } from "./components/Thread/index"
-import { CardPost } from "./components/Home"
 import { HomePosts } from "./components/Home/index"
 import PostUpdate from "./components/Editor/updatePost"
-import { CategoryButtons } from "./components/Category/buttonCategory"
 import { CategorizedPosts } from "./components/Category/categoryPost"
 import { LoginBox } from "./components/Login/loginBox"
 
@@ -24,14 +20,23 @@ function App() {
         <Route path = "/posts/:id" element = {<ViewThread/>}></Route>
         <Route path = "/posts/:id/edit" element = {<PostUpdate/>}></Route>
         <Route path = "new" element = { <PostCreator/>}></Route>
-        <Route path= "/posts/category/news" element={CategorizedPosts({ value: "news" })}></Route>
-        <Route path= "/posts/category/entertainment" element={CategorizedPosts({ value: "entertainment" })}></Route>
-        <Route path = "/posts/category/food" element={CategorizedPosts({ value: "food" })}></Route>
-        <Route path = "/posts/category/study" element={CategorizedPosts({ value: "study" })}></Route> 
-        <Route path = "/posts/category/sports" element={CategorizedPosts({ value: "sports" })}></Route>
+        {categories.map(category => (
+          <Route
+            key={category.path}
+            path={`/posts/category/${category.path}`}
+            element={<CategorizedPosts value={category.value} />}/>
+        ))} 
       </Routes>
     </div>
   );
 };
+
+const categories = [
+  { path: 'news', value: 'news' },
+  { path: 'entertainment', value: 'entertainment' },
+  { path: 'food', value: 'food' },
+  { path: 'study', value: 'study' },
+  { path: 'sports', value: 'sports' },
+];
 
 export default App;
